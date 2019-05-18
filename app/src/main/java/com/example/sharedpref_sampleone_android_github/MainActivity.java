fd6888b4832a1d46e4bb8196c4855e69e12f032b
Aman Sharma <amansharma.11@hotmail.com>
2019-05-18 15:41:05 +0530

    commit

--- app/src/main/java/com/example/sharedpref_sampleone_android_github/MainActivity.java
+++ app/src/main/java/com/example/sharedpref_sampleone_android_github/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.sharedpref_sampleone_android_github;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int SIGNIN_REQUEST = 1001;
+    public static final String MY_PREFS = "myPrefs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +35,19 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode == SIGNIN_REQUEST){
+            String email = data.getStringExtra(SigninActivity.EMAIL_KEY);
+            Toast.makeText(getApplicationContext(),"EmailId is :- "+email,Toast.LENGTH_LONG).show();
+
+            SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS,MODE_PRIVATE).edit();
+            editor.putString(SigninActivity.EMAIL_KEY,email);
+            editor.apply();
+        }
     }
 }
--- app/src/main/java/com/example/sharedpref_sampleone_android_github/SigninActivity.java
+++ app/src/main/java/com/example/sharedpref_sampleone_android_github/SigninActivity.java
@@ -1,13 +1,70 @@
 package com.example.sharedpref_sampleone_android_github;
 
+import android.content.SharedPreferences;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class SigninActivity extends AppCompatActivity {
+
+
+    public static final String EMAIL_KEY = "email_key";
+
+    private EditText email,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        email = findViewById(R.id.email_et);
+        password = findViewById(R.id.password_et);
+
+        SharedPreferences sharedPreferences = getSharedPreferences(MainActivity.MY_PREFS,MODE_PRIVATE);
+
+        String userEmail = sharedPreferences.getString(EMAIL_KEY,"");
+
+
+        if(!TextUtils.isEmpty(userEmail)){
+
+            email.setText(userEmail);
+
+        }
+    }
+
+    public void signIn(View view) {
+
+        String user_email = email.getText().toString();
+        String user_password = password.getText().toString();
+
+        if(TextUtils.isEmpty(user_email) || !isEmailValid(user_email)){
+
+            Snackbar.make(view,"Enter Valid Email Id", Snackbar.LENGTH_SHORT).setAction("ok",null).show();
+        }
+        else if(TextUtils.isEmpty(user_password) || !isPasswordValid(user_password)){
+
+            Snackbar.make(view,"Enter Valid Password", Snackbar.LENGTH_SHORT).setAction("ok",null).show();
+        }
+        else{
+            getIntent().putExtra(EMAIL_KEY,user_email);
+            setResult(RESULT_OK,getIntent());
+            finish();
+        }
+
+    }
+
+    private boolean isEmailValid(String email){
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password){
+        return password.length()>4;
     }
 }
