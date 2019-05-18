437b9d729e797604973e40c28f43148d93b415a9
Aman Sharma <amansharma.11@hotmail.com>
2019-05-18 14:17:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sharedpref_sampleone_android_github/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.sharedpref_sampleone_android_github;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int SIGNIN_REQUEST = 1001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_signin:
+                Intent intent = new Intent(this,SigninActivity.class);
+                startActivityForResult(intent,SIGNIN_REQUEST);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
